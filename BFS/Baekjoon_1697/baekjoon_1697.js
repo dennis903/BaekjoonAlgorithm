42ce1a9df048514629824a13ffdcd5ae0fcc5fc3
이형진 <hyeongjin3@hanmail.net>
2022-05-10 10:16:39 +0900

    baekjoon_1697 finished

--- BFS/Baekjoon_1697/baekjoon_1697.js
+++ BFS/Baekjoon_1697/baekjoon_1697.js
@@ -1,19 +1,17 @@
 let input = require('fs').readFileSync('input.txt').toString().split('\n');
 const [subinsLocation, brosLocation] = input.shift().split(" ").map(e => +e);
 let map = Array(200001).fill(0);
-let visited = Array(200001).fill(0);
+let visited = Array(200001).fill(-1);
 let queue = [];
 map[brosLocation] = 1;
 const bfs = () => {
-	let dist = 0;
-	let curLocation = subinsLocation;
-	queue.push(curLocation);
-	visited[curLocation] = 1;
+	let [curLocation, dist] = [subinsLocation, 0];
+	queue.push([curLocation, dist]);
+	visited[curLocation] = dist;
 	while (queue.length) {
-		curLocation = queue.shift();
+		const [curLocation, dist] = queue.shift();
 		if (map[curLocation] === 1)
-			return (visited[curLocation]);
-		dist++;
+			return (dist);
 		for (let i = 0; i < 3; i++) {
 			let newLocation = curLocation;
 			if (i === 0)
@@ -22,9 +20,9 @@ const bfs = () => {
 				newLocation = curLocation + 1;
 			else
 				newLocation = curLocation * 2;
-			if (newLocation > -1 && newLocation <= 200000 && visited[newLocation] === 0) {
-				queue.push(newLocation);
-				visited[newLocation] = dist;
+			if (newLocation > -1 && newLocation <= 200000 && visited[newLocation] === -1) {
+				queue.push([newLocation, dist + 1]);
+				visited[newLocation] = 1;
 			}
 		}
 	}
