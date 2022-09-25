421942dc22a78ffa516bae8d836bbe081239eb7e
dennis903 <hyeongjin3@hanmail.net>
2022-09-25 19:14:35 +0900

    baekjoon_10026 finished

--- BFS/Baekjoon_10026/baekjoon_10026.js
+++ BFS/Baekjoon_10026/baekjoon_10026.js
@@ -7,6 +7,11 @@ let input = require("fs")
 const N = +input.shift();
 
 const map = [];
+
+const visited1 = Array.from(Array(N), () => Array(N).fill(false));
+const visited2 = Array.from(Array(N), () => Array(N).fill(false));
+let count1 = 0;
+let count2 = 0;
 input.map((e) => {
   const newArr = e.split("");
   map.push(newArr);
@@ -18,3 +23,44 @@ const direction = [
   [1, 0],
   [0, -1],
 ];
+
+const bfs = (isBlind, y, x, color) => {
+  const queue = [];
+
+  queue.push([y, x]);
+  while (queue.length) {
+    const [curY, curX] = queue.shift();
+    for (let i = 0; i < 4; i++) {
+      const [ny, nx] = [curY + direction[i][0], curX + direction[i][1]];
+      if (ny < 0 || ny >= N || nx < 0 || nx >= N) continue;
+      if (isBlind) {
+        if (visited2[ny][nx] === true) continue;
+        if ((map[ny][nx] === "R" || map[ny][nx] === "G") && color === "B")
+          continue;
+        if (map[ny][nx] === "B" && (color === "R" || color === "G")) continue;
+        queue.push([ny, nx]);
+        visited2[ny][nx] = true;
+      } else {
+        if (visited1[ny][nx] === true) continue;
+        if (map[ny][nx] !== color) continue;
+        queue.push([ny, nx]);
+        visited1[ny][nx] = true;
+      }
+    }
+  }
+};
+
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < N; j++) {
+    if (!visited1[i][j]) {
+      count1++;
+      bfs(false, i, j, map[i][j]);
+    }
+    if (!visited2[i][j]) {
+      count2++;
+      bfs(true, i, j, map[i][j]);
+    }
+  }
+}
+
+console.log(count1, count2);
